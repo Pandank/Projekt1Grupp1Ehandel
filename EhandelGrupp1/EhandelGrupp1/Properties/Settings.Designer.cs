2e736a72fa54b2f0717e1b77b4ab109254bdd0f0
Victor Öman <Victoroman2@gmail.com>
2017-03-23 11:53:42 +0100

    removed cnstr

--- EhandelGrupp1/EhandelGrupp1/Properties/Settings.Designer.cs
+++ EhandelGrupp1/EhandelGrupp1/Properties/Settings.Designer.cs
@@ -22,16 +22,5 @@ namespace EhandelGrupp1.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=213.114.120.91;Initial Catalog=eHandel;Persist Security Info=True;Use" +
-            "r ID=ehande;Password=ehandel890807")]
-        public string cnstr {
-            get {
-                return ((string)(this["cnstr"]));
-            }
-        }
     }
 }
