11cd73f42937bbdc51dbe999877d70ae5e8299c1
Victor Öman <Victoroman2@gmail.com>
2017-03-23 12:07:23 +0100

    changed props

--- EhandelGrupp1/EhandelGrupp1/Properties/Settings.Designer.cs
+++ EhandelGrupp1/EhandelGrupp1/Properties/Settings.Designer.cs
@@ -22,5 +22,15 @@ namespace EhandelGrupp1.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("dummyCONSTRING")]
+        public string conSTR {
+            get {
+                return ((string)(this["conSTR"]));
+            }
+        }
     }
 }
