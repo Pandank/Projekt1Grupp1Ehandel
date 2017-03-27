bd64b84fdac76e7421cdd9e378a6a257073ccc74
Carolina <Administrator@ACADEMY-6303PKC>
2017-03-27 16:02:58 +0200

    Added srv-allorders

---
+++ EhandelGrupp1/EhandelGrupp1/services/svc-allorders.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EhandelGrupp1;
+
+namespace EhandelGrupp1.services
+{
+    public partial class svc_allorders : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var allOrders = DataManagement.GetAllOrders();
+            LiteralAllOrders.Text = allOrders;
+        }
+    }
+}
