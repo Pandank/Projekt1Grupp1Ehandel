21b967877176c6f39851e3fac84a73970d6f921c
Carolina <Administrator@ACADEMY-6303PKC>
2017-03-27 15:39:11 +0200

    Added service svc_profile

---
+++ EhandelGrupp1/EhandelGrupp1/services/svc-profile.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EhandelGrupp1;
+
+namespace EhandelGrupp1.services
+{
+    public partial class svc_profile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Request["userId"]))
+            {
+                var user = DataManagement.GetCustomerByID(int.Parse(Request["userId"]));
+                LiteralProfile.Text = user;
+            }
+        }
+    }
+}
