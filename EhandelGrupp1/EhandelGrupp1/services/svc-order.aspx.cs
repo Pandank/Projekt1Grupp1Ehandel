8b6cbfc4c6947d0f137b5a22450487f7146eaeae
Carolina <Administrator@ACADEMY-6303PKC>
2017-03-27 15:49:45 +0200

    Added svc-order

---
+++ EhandelGrupp1/EhandelGrupp1/services/svc-order.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EhandelGrupp1;
+
+namespace EhandelGrupp1.services
+{
+    public partial class svc_order : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Request["userId"]))
+            {
+                var order = DataManagement.GetAllOdersByCustomerID(int.Parse(Request["userId"]));
+                LiteralOrder.Text = order;
+            }
+
+        }
+    }
+}
